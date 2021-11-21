9390b3a5b89db3138e014ab171ccf8fc80ef2d9c
mansurissa <mansurissa6@gmail.com>
2021-11-21 19:21:46 +0200

    Conventing from celisius to fahrneight and vise versa

---
+++ conventer.js
@@ -0,0 +1,26 @@
+const { program } = require('commander');
+program.version('0.0.1');
+
+program
+  .command('cToF')
+  .description('Convent from Celsius to Fahrenheit ')
+  .action(() => {
+    const cTemp = process.argv.slice(2)[1];
+    const cToFahr = (cTemp * 9) / 5 + 32;
+    const message =
+      cTemp + '\xB0C is equal to ' + cToFahr.toFixed(2) + '\xB0F.';
+    console.log(message);
+    return message;
+  });
+program
+  .command('fToC')
+  .description('Convent from Fahrenheit to  Celsius')
+  .action(() => {
+    const fTemp = process.argv.slice(2)[1];
+    const fToCel = ((fTemp - 32) * 5) / 9;
+    const message = fTemp + '\xB0F is equal to ' + fToCel.toFixed(2) + '\xB0C.';
+    console.log(message);
+    return message;
+  });
+
+program.parse();
